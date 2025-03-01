717b452bd2d630e5b1a4c37bfe9d3be334e3d5af
H82M19\Student <Student>
2025-03-01 08:42:09 +0700

    Example Singleton da luong

--- BuiThanhThinh_21027341_ExampleDP/src/main/ThreadSafeSingleton.java
+++ BuiThanhThinh_21027341_ExampleDP/src/main/ThreadSafeSingleton.java
@@ -1,12 +1,14 @@
 package main;
 
 public class ThreadSafeSingleton {
-	private static ThreadSafeSingleton instance;
-	private ThreadSafeSingleton(){}
-	public static synchronized ThreadSafeSingleton getInstance(){
-	if(instance == null){
-	instance = new ThreadSafeSingleton();
-	}
-	return instance;
-	}
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {}
+
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
 }
--- BuiThanhThinh_21027341_ExampleDP/src/main/main.java
+++ BuiThanhThinh_21027341_ExampleDP/src/main/main.java
@@ -24,13 +24,39 @@ public class main {
 //      // In ra mã hash của hai đối tượng, nếu giống nhau thì chắc chắn chỉ có một thể hiện.
 //      System.out.println("Instance 1: " + instance1.hashCode());
 //      System.out.println("Instance 2: " + instance2.hashCode());
-    	StaticBlockSingleton instance1 = StaticBlockSingleton.getInstance();
-    	StaticBlockSingleton instance2 = StaticBlockSingleton.getInstance();
+//    	StaticBlockSingleton instance1 = StaticBlockSingleton.getInstance();
+//    	StaticBlockSingleton instance2 = StaticBlockSingleton.getInstance();
+//
+//        // In ra mã hash của hai đối tượng, nếu giống nhau thì chắc chắn chỉ có một thể hiện.
+//        System.out.println("Instance 1: " + instance1.hashCode());
+//        System.out.println("Instance 2: " + instance2.hashCode());
+//        
+     // Tạo hai luồng để kiểm tra
+        Thread thread1 = new Thread(() -> {
+            ThreadSafeSingleton instance1 = ThreadSafeSingleton.getInstance();
+            System.out.println("Thread 1: " + instance1.hashCode());
+        });
 
-        // In ra mã hash của hai đối tượng, nếu giống nhau thì chắc chắn chỉ có một thể hiện.
-        System.out.println("Instance 1: " + instance1.hashCode());
-        System.out.println("Instance 2: " + instance2.hashCode());
-        
-       
+        Thread thread2 = new Thread(() -> {
+            ThreadSafeSingleton instance2 = ThreadSafeSingleton.getInstance();
+            System.out.println("Thread 2: " + instance2.hashCode());
+        });
+
+        // Khởi động các luồng
+        thread1.start();
+        thread2.start();
+
+        // Chờ cho các luồng kết thúc
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Kiểm tra lại trong main thread
+        ThreadSafeSingleton mainInstance = ThreadSafeSingleton.getInstance();
+        System.out.println("Main Thread: " + mainInstance.hashCode());
     }
+    
 }
