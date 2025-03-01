8a6e0e4252af792384d028eeca7fc39d47b2a60f
H82M19\Student <Student>
2025-03-01 08:17:23 +0700

    Example BillPughSingleton

--- BuiThanhThinh_21027341_ExampleDP/src/main/main.java
+++ BuiThanhThinh_21027341_ExampleDP/src/main/main.java
@@ -2,12 +2,19 @@ package main;
 
 public class main {
     public static void main(String[] args) {
-        ThreadSafeSingleton instance1 = ThreadSafeSingleton.getInstance();
-        ThreadSafeSingleton instance2 = ThreadSafeSingleton.getInstance();
+//        ThreadSafeSingleton instance1 = ThreadSafeSingleton.getInstance();
+//        ThreadSafeSingleton instance2 = ThreadSafeSingleton.getInstance();
+//        
+//        System.out.println("Instance 1: " + instance1.hashCode());
+//        System.out.println("Instance 2: " + instance2.hashCode());
         
-        System.out.println("Instance 1: " + instance1.hashCode());
-        System.out.println("Instance 2: " + instance2.hashCode());
+      BillPughSingleton instance1 = BillPughSingleton.getInstance();
+      BillPughSingleton instance2 = BillPughSingleton.getInstance();
+      
+      System.out.println("Instance 1: " + instance1.hashCode());
+      System.out.println("Instance 2: " + instance2.hashCode());
         
+
         
     }
 }
